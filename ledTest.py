1b4c860d4ce1ea485c3a13dba9a08faa45cf2e40
Terren Zhou <terren.zhou@outlook.com>
2018-11-24 16:56:59 +0800

    aaa

--- ledTest.py
+++ ledTest.py
@@ -1,17 +1,14 @@
 import gpiozero
 from time import sleep
 
-led1 = gpiozero.LED(18)
-led2 = gpiozero.LED(23)
+
 led3 = gpiozero.LED(24)
 
 while True:
-    led1.on()
-    led2.on()
+
     led3.on()
     sleep(1)
-    led1.off()
-    led2.off()
+
     led3.off()
     sleep(1)
 
